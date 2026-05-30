db9c931dfb0b257851923f642d850dd92c707aac
georgegoldman <georgegoldmanjohn.o@gmail.com>
2026-05-30 19:19:15 +0100

    build

--- cli/src/sui/deep.ts
+++ cli/src/sui/deep.ts
@@ -1,6 +1,12 @@
+/*
+prices are provided in standard decimal format
+such as 10.5 SUI or 0.00001 nBTC
+
+*/
+
 import { SuiGrpcClient } from "@mysten/sui/grpc";
-import { deepbook } from "@mysten/deepbook-v3";
-import { type Transaction } from "@mysten/sui/transactions";
+import { type AccountInfo, deepbook } from "@mysten/deepbook-v3";
+import { Transaction } from "@mysten/sui/transactions";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import dotenv from "dotenv";
 dotenv.config();
@@ -8,6 +14,10 @@ dotenv.config();
 export class DeepBookService {
   private _keypair = Ed25519Keypair.fromSecretKey(process.env.PK!);
   tx: Transaction;
+
+  constructor() {
+    this.tx = new Transaction();
+  }
 
   grpcClient = new SuiGrpcClient({
     network: "testnet",
@@ -105,4 +115,44 @@ export class DeepBookService {
     this.tx.transferObjects([depositCap], recipient);
     return this.tx;
   }
+
+  /*
+    Use account to retrieve the account information for a BalanceManager in a pool,
+    which has the following form:
+    parameters -
+      @poolKey: String that identifies the pool to query.
+      @balanceManagerKey: key of the balance manager defined in the SDK.
+  */
+  async account(poolKey: string, managerKey: string): Promise<AccountInfo> {
+    return this.grpcClient.deepbook.account(poolKey, managerKey);
+  }
+
+  /*
+    Use accountOpenOrders to retrieve open orders for the balance manager and pool with the IDs you provide.
+    The call returns a Promise that contains an array of open order IDs.
+    parameters -
+      poolKey: String that identifies the pool to query.
+      managerKey: String that identifies the balance manager to query.
+  */
+
+  async accountOpenOrders(
+    poolKey: string,
+    managerKey: string,
+  ): Promise<string[]> {
+    return this.grpcClient.deepbook.accountOpenOrders(poolKey, managerKey);
+  }
+
+  /*
+  Use checkManagerBalance to check the balance manager for a specific coin.
+  The call returns a Promise in the form:
+    {
+      coinType: string,
+      balance: number
+    }
+
+    Parameters
+
+    managerKey: String that identifies the balance manager to query.
+    coinKey: String that identifies the coin to query the balance of.
+  */
 }
